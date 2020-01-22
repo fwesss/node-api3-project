902f33064d31f91fe3ebd67092bc2cb95fb6b9a7
Westley Feller <feller.westley@gmail.com>
2020-01-21 22:08:42 -0800

    Log reqest method, url, and timestamp

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ import express from 'express'
 
 const server = express()
 
+// custom middleware
+
+const logger = (req, _res, next) => {
+  console.log(`${req.method} to ${req.url} at ${Date()}`)
+  next()
+}
+
+server.use(logger)
+
 server.get('/', (_req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 })
