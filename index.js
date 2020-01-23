23e2def7a85f4c09ec38cc601b366343fafa96cc
Westley Feller <feller.westley@gmail.com>
2020-01-22 21:13:20 -0800

    dotenv added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import server from './server'
 
+const port = process.env.PORT || 5000
+
 server.listen(4000, () =>
-  console.info('\n*** Server Running on http://localhost:4000 ***\n')
+  console.info(`\n*** Server Running on http://localhost:${port} ***\n`)
 )
